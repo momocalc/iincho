a2fa46db19470351b1e4703f2d3031220d1af7b9
shiccocsan <petitlycee11235@gmail.com>
2016-04-06 08:07:06 +0900

    categoryモデルをadmin管理対象に追加

--- categories/models.py
+++ categories/models.py
@@ -7,3 +7,6 @@ from core.models import TimeStampedModel
 class Category(TimeStampedModel):
     name = models.TextField(unique=True)
     sort_number = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
