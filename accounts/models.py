99c1319bbf2a78e7fb03617a100893548e9fda9c
shiccocsan <petitlycee11235@gmail.com>
2016-04-20 08:32:20 +0900

    プロファイル画像の管理をURLからFileに変更

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,9 @@ from core.models import TimeStampedModel
 from django.contrib.auth.models import User
 
 
+PHOTO_UPLOAD_TO = 'accounts/'
+
+
 class Profile(TimeStampedModel):
     user = models.OneToOneField(User)
-    photo = models.URLField(blank=True)
+    photo = models.FileField(blank=True, upload_to=PHOTO_UPLOAD_TO)
--- accounts/pipelines.py
+++ accounts/pipelines.py
@@ -12,25 +12,28 @@ def ensure_sub_dir(*dir_names):
     return res
 
 
-def image_download(url, user_id):
-
+def download_user_avator(url):
+    import uuid
     ext = os.path.splitext(
         os.path.basename(url).split('?')[0])[1]
-    image_name = str(user_id) + str(ext)
+    image_name = str(uuid.uuid1()) + ext
 
     tmp_path = os.path.join(ensure_sub_dir('tmp'), image_name)
     local_file = open(tmp_path, 'wb')
 
     img = urllib.request.urlopen(url)
     local_file.write(img.read())
-    img.close()
     local_file.close()
+    img.close()
+    return tmp_path, ext
 
-    # プロファイル画像ディレクトリに移動
-    image_path = os.path.join(ensure_sub_dir('images', 'icon'), image_name)
-    os.rename(tmp_path, image_path)
-    return image_name
 
+def __set_avatar(url, user):
+    from django.core.files import File
+    # local_file = open(tmp_path)
+    tmp_path, ext = download_user_avator(url)
+    f = open(tmp_path, 'rb')
+    user.profile.photo.save(user.username + ext, File(f))
 
 def get_user_avatar(strategy, user, response, is_new=False, *args, **kwargs):
 
@@ -40,12 +43,6 @@ def get_user_avatar(strategy, user, response, is_new=False, *args, **kwargs):
     if not hasattr(user, 'profile'):
         Profile(user=user).save()
 
-    if is_new and isinstance(kwargs['backend'], GoogleOAuth2):
-        try:
-            image_name = image_download(response['image']['url'], user.id)
-            if image_name:
-                user.profile.photo = image_name
-                user.profile.save()
-
-        except Exception:
-            pass
+    if isinstance(kwargs['backend'], GoogleOAuth2) and not user.profile.photo:
+        __set_avatar(response['image']['url'], user)
+        user.profile.save()
